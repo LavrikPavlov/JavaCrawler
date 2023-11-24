4c5ca7cc2141a6e8aa8ab34f959098dec183194b
misha <misha-kazan2013@yandex.ru>
2023-11-25 03:07:43 +0700

    upload

--- src/main/java/ru/crawler/App.java
+++ src/main/java/ru/crawler/App.java
@@ -1,20 +1,11 @@
 package ru.crawler;
 
 
-import ru.crawler.classes.Crawler;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class App
 {
 
     public static void main(String[] args) {
-        Crawler crawler = new Crawler("Crawler.db");
-        crawler.initDB();
-        List<String> urlList = new ArrayList<>();
-        urlList.add("https://lenta.ru");
-        crawler.crawl(urlList, 1);
+        System.out.println("Лабораторные работы по Интеликтуальные Системы");
     }
 
 }
